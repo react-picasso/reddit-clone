b9dffd6e354083905f46afe928e2243018b1adc9
Prathamesh Kulkarni <kulkarni@fim.uni-passau.de>
2023-12-08 11:13:41 +0100

    Added navbar and home layout

---
+++ src/containers/Home/Home.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "./Home.scss";
+
+export interface HomeProps {}
+
+export default function Home() {
+    return (
+        <div>
+            <h1>This is the home page.</h1>
+        </div>
+    )
+}
---
+++ src/containers/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "./Navbar.scss";
+
+export interface HomeProps {}
+
+export default function Home() {
+    return (
+        <div>
+            <h1>This is the navbar.</h1>
+        </div>
+    )
+}
